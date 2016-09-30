e9956d650a8120ad6c4ff79195dc6a77c77acd1c
chenhaoyang <chenhaoyang0529@gmail.com>
2016-09-30 17:23:53 +0800

    add demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,5 +1,27 @@
 angular.module('demo', ['ng.treeView'])
   .controller('DemoController', ['$scope',
     function ($scope) {
+      $scope.testInputModel = {};
 
-  }]);
+      $scope.testSourceModel = {
+        foo: true,
+        bar: true
+      };
+
+      $scope.test = {
+        // component model
+        inputModel: 'testInputModel',
+
+        // source model
+        sourceModel: 'testSourceModel',
+
+        // input style
+        inputStyle: 'test-input',
+
+        // label style
+        labelStyle: 'test-label',
+
+        // wrapper style
+        wrapperStyle: 'test-wrapper'
+      };
+    }]);
