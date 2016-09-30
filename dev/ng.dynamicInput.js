c3eb3b0c3eb400a8a935d245d7b09bfc36b6b5cd
chenhaoyang <chenhaoyang0529@gmail.com>
2016-09-30 15:41:27 +0800

    module

--- dev/ng.dynamicInput.js
+++ dev/ng.dynamicInput.js
@@ -1,52 +1,53 @@
+angular.module('ng.treeView', [])
 // dynamic input
-app.directive('dynamicInput', ['$compile', function ($compile) {
-  return {
-    restrict: 'A',
-    link: function (scope, element, attrs) {
-      // component id
-      var inputId = attrs.inputId;
+  .directive('dynamicInput', ['$compile', function ($compile) {
+    return {
+      restrict: 'A',
+      link: function (scope, element, attrs) {
+        // component id
+        var inputId = attrs.inputId;
 
-      // component model
-      var inputModel = attrs.inputModel || scope[inputId].inputModel;
+        // component model
+        var inputModel = attrs.inputModel || scope[inputId].inputModel;
 
-      // source model
-      var sourceModel = scope[inputId].sourceModel;
+        // source model
+        var sourceModel = scope[inputId].sourceModel;
 
-      // input style
-      var inputStyle = attrs.inputStyle || scope[inputId].inputStyle || '';
+        // input style
+        var inputStyle = attrs.inputStyle || scope[inputId].inputStyle || '';
 
-      // label style
-      var labelStyle = attrs.labelStyle || scope[inputId].labelStyle || '';
+        // label style
+        var labelStyle = attrs.labelStyle || scope[inputId].labelStyle || '';
 
-      // wrapper style
-      var wrapperStyle = attrs.wrapperStyle || scope[inputId].wrapperStyle || '';
+        // wrapper style
+        var wrapperStyle = attrs.wrapperStyle || scope[inputId].wrapperStyle || '';
 
-      // template
-      var template = '';
+        // template
+        var template = '';
 
-      // check input id, component model, source model
-      if (inputId && inputModel && sourceModel) {
+        // check input id, component model, source model
+        if (inputId && inputModel && sourceModel) {
 
-        // create inputId object if not exists
-        scope[inputId] = scope[inputId] || {};
+          // create inputId object if not exists
+          scope[inputId] = scope[inputId] || {};
 
-        // set template
-        angular.forEach(sourceModel, function (value, key) {
-          if (value) {
-            template = template +
-              '<div class="' + wrapperStyle + '">'+
-              '<label class="' + labelStyle + '">' + key + '</label>' +
-              '<input type="text" class="' + inputStyle + '" ' +
-              'data-ng-model="' + inputModel + '.' + key + '" >' +
-              '</div>';
-          }
-        });
+          // set template
+          angular.forEach(sourceModel, function (value, key) {
+            if (value) {
+              template = template +
+                '<div class="' + wrapperStyle + '">' +
+                '<label class="' + labelStyle + '">' + key + '</label>' +
+                '<input type="text" class="' + inputStyle + '" ' +
+                'data-ng-model="' + inputModel + '.' + key + '" >' +
+                '</div>';
+            }
+          });
 
-        element.html('').append($compile(template)(scope));
-      } else {
-        console.error('must set inputId && inputModel && sourceModel');
+          element.html('').append($compile(template)(scope));
+        } else {
+          console.error('must set inputId && inputModel && sourceModel');
+        }
+
       }
-
-    }
-  };
-}]);
+    };
+  }]);
